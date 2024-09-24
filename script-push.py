b90880fc18436969bca14bde6ba8fd77dab2f801
Koding Next Samarinda <samarinda@kodingnext.com>
2024-09-24 15:59:04 +0800

    Add Kandika Prima Project by Putra

--- script-push.py
+++ script-push.py
@@ -2,6 +2,8 @@ import git
 
 studentName = "Kandika Prima"
 teacherName = "Putra"
+
+
 local_repo_path = "C:/Github Koding Next/Python-Arcade"
 
 def git_pull_push(repo_path, student, teacher):
@@ -10,7 +12,7 @@ def git_pull_push(repo_path, student, teacher):
 
         repo.git.execute("git pull")
         repo.git.execute("git add .")
-        repo.git.execute(f'''git commit -m "Add Python Script"''')
+        repo.git.execute(f'''git commit -m "Add {student} Project by {teacher}"''')
         repo.git.execute("git push")
 
         print(f"File project {student} pada folder {repo_path} berhasil di upload")
